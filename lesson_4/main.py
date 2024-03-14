49cb563118ee9db43e52b153fccfa05a1aef6f33
Nikita_Kukharskiy <kukharskiy.nikita@mail.ru>
2024-03-14 14:02:44 +0300

    hot fix ht-4

--- lesson_4/main.py
+++ lesson_4/main.py
@@ -22,27 +22,30 @@ print(f'\nЗадание 1.3\n{new_list1}\n{new_list2}')
 
 
 # Задание 2 - Создать два списка, с одинаковым кол-вом элементов. Сделать из этих списков словарь
+print("Zen of Python")
 key_list = [i for i in range(1, 6)]
 value_list = ['Belarus', 'Minsk', 'Kukharskiy', 'Nikita', 'Igorevich']
 new_dict = dict(zip(key_list, value_list))
 
-print(f'\nЗадание 2\n{new_dict}')
+print(f'\nЗадание 2\n{new_dict}\n')
 
 
 # Задание 3.1 Определите количество строк в Дзене Питона.
-zen_file = open('zen.txt', 'r')
-lines = len(zen_file.readlines())
+print('Zen of Python'.center(225, '-'))
+import this
+zen_python = "".join([this.d.get(c, c) for c in this.s])
+lines = len(zen_python.splitlines())
 
-print(f'\nЗадание 3.1\nКоличество строк в файле: {lines}')
+print(f'\nЗадание 3.1\nКоличество строк в zen of Python: {lines}')
 
 # Задание 3.2 Подсчитайте количество вхождений ключевых слов из Дзена Питона, таких как "is", "and", "or". Сложить в
 # словарь такого типа {“is”: 4, “and”: None, “or”: 100}
-new_file = open('zen.txt').read()
-count_is = new_file.count('is')
-count_and = new_file.count('and')
-count_or = new_file.count('or')
+count_is = zen_python.count('is')
+count_and = zen_python.count('and')
+count_or = zen_python.count('or')
 
 zen_dict = {}
+
 zen_dict['is'] = count_is
 zen_dict['and'] = count_and
 zen_dict['or'] = count_or
@@ -51,7 +54,6 @@ print(f'\nЗадание 3.2\n{zen_dict}')
 
 
 # Задание 3.3 Замените все вхождения слова "is" в Дзене Питона на "is not".
-print(f'\nЗадание 3.2\n', new_file.replace("is", "is not"))
+print(f'\nЗадание 3.3\n', zen_python.replace("is", "is not"))
 
-# такое ощущение, что я использовал слишком простой способ в этом задании, не такой, какой должен был
 
